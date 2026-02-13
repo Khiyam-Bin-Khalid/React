90145b295bd6a4a1fa57fd7164989c30b12d0393
Khiyam-Bin-Khalid <khiyambinkhalid06@gmail.com>
2026-02-13 15:33:50 -0800

    Redux tool-Kit

---
+++ Redux-toolkit/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import AddTodo from './components/AddTodo'
+import './App.css'
+import Todos from './components/Todos'
+
+function App() {
+
+
+  return (
+    <>
+    <AddTodo/>
+    <Todos></Todos>
+    </>
+  )
+}
+
+export default App
